46133d158f04a3f7e62b400abb09dac0968c9e58
Ahmed El.Hussaini <aelhussaini@gmail.com>
2019-01-26 16:36:33 -0500

    Replicate alias_method_chain

--- lib/active_model_serializers/cancan/associations.rb
+++ lib/active_model_serializers/cancan/associations.rb
@@ -1,31 +1,35 @@
 module ActiveModel
   class Serializer
-    class Association #:nodoc:
+    module Associations #:nodoc:
+      class Config #:nodoc:
 
-      def authorize?
-        !!options[:authorize]
+        def authorize?
+          !!option(:authorize)
+        end
+
       end
 
       class HasMany #:nodoc:
 
         def serialize_with_cancan
           return serialize_without_cancan unless authorize?
-          object.select {|item| find_serializable(item).can?(:read, item) }.map do |item|
+          associated_object.select{|o| source_serializer.can?(:read, o)}.map do |item|
             find_serializable(item).serializable_hash
           end
         end
-        alias_method_chain :serialize, :cancan
+        alias_method :serialize_without_cancan, :serialize
+        alias_method :serialize, :serialize_with_cancan
 
       end
 
       class HasOne #:nodoc:
 
         def serialize_with_cancan
-          serializer = find_serializable(object)
-          return nil unless !authorize? || serializer && serializer.can?(:read, object)
+          return nil unless !authorize? || source_serializer.can?(:read, associated_object)
           serialize_without_cancan
         end
-        alias_method_chain :serialize, :cancan
+        alias_method :serialize_without_cancan, :serialize
+        alias_method :serialize, :serialize_with_cancan
 
       end
     end
